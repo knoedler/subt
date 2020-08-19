db0e1cb87c1b34e9dc0939dc713bf52349ce4a7b
Nate Koenig <nate@openrobotics.org>
2020-08-19 14:52:03 -0700

    Working on support for worlds in launch files
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

---
+++ subt_ign/launch/world_map.rb
@@ -0,0 +1,51 @@
+$worldNameMap={
+  "cave_circuit_practice_01" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Cave Circuit Practice 01",
+  "cave_circuit_practice_02" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Cave Circuit Practice 02",
+  "cave_circuit_practice_03" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Cave Circuit Practice 03",
+
+  "tunnel_circuit_practice_01" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Tunnel Circuit Practice 01",
+  "tunnel_circuit_practice_02" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Tunnel Circuit Practice 02",
+  "tunnel_circuit_practice_03" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Tunnel Circuit Practice 03",
+
+  "urban_circuit_practice_01" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit Practice 01",
+  "urban_circuit_practice_02" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit Practice 02",
+  "urban_circuit_practice_03" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit Practice 03",
+
+  "simple_cave_01" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Simple Cave 01",
+  "simple_cave_02" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Simple Cave 02",
+  "simple_cave_03" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Simple Cave 03",
+
+  "simple_tunnel_01" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Simple Tunnel 01",
+  "simple_tunnel_02" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Simple Tunnel 02",
+  "simple_tunnel_03" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Simple Tunnel 03",
+
+  "simple_urban_01" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Simple Urban 01",
+  "simple_urban_02" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Simple Urban 02",
+  "simple_urban_03" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Simple Urban 03",
+
+  "cave_qual" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Cave Qual",
+  "tunnel_qual" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Tunnel Qual",
+  "urban_qual" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Qual",
+
+  "niosh_ex_config_a" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/NIOSH EX Config A",
+  "niosh_ex_config_b" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/NIOSH EX Config B",
+  "niosh_sr_config_a" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/NIOSH SR Config A",
+  "niosh_sr_config_b" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/NIOSH SR Config B",
+
+  "virtual_stix" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Virtual STIX",
+
+  "tunnel_circuit_01" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Tunnel Circuit 01",
+  "tunnel_circuit_07" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Tunnel Circuit 07",
+  "tunnel_circuit_12" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Tunnel Circuit 12",
+  "tunnel_circuit_13" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Tunnel Circuit 13",
+  "tunnel_circuit_15" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Tunnel Circuit 15",
+
+  "urban_circuit_01" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit 01",
+  "urban_circuit_02" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit 02",
+  "urban_circuit_03" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit 03",
+  "urban_circuit_04" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit 04",
+  "urban_circuit_05" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit 05",
+  "urban_circuit_06" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit 06",
+  "urban_circuit_07" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit 07",
+  "urban_circuit_08" => "https://fuel.ignitionrobotics.org/OpenRobotics/worlds/Urban Circuit 08",
+}
